9e371a15fa87093f936c14a5558dee2e1e1da4b9
araoalves <arao.alves@ceuma.br>
2017-05-16 17:19:04 -0300

    Ajustando Layout

--- src/services/configuracoes/ServiceConfiguration.ts
+++ src/services/configuracoes/ServiceConfiguration.ts
@@ -1,9 +1,9 @@
 export class ServiceConfiguration{
     
-    private _url:string = "http://localhost:85/";
-    //private _url:string = "http://www.nodeti.com.br/";
-    private _api:string = "GIT/opiniometro/api/";
-    //private _api:string = "opiniometro/";
+    //private _url:string = "http://localhost:85/";
+    private _url:string = "http://www.nodeti.com.br/";
+    //private _api:string = "GIT/opiniometro/api/";
+    private _api:string = "opiniometro/api/";
 
     public RECUPERAR_USUARIO: string = "login"
     public CADASTRAR_OPINIAO: string = 'cadastro/cadastrarOpiniao';
